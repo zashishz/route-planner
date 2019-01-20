96f04c5acc93a7071ab7667c023aaa75cd7c3f79
Ashish <ashishrodio@gmail.com>
2019-01-21 03:10:01 +0530

    Added direction Service to maps

--- src/routes/components/RouteMap/RouteMap.js
+++ src/routes/components/RouteMap/RouteMap.js
@@ -22,6 +22,46 @@ class RouteMap extends Component {
         this.init();
     }
 
+    componentDidUpdate() {
+        const { directions } = this.props;
+        if (directions) {
+            this.renderDirections(directions);
+        }
+    }
+
+    interpolatePositions = path => {
+        return path.map(([lat, lng]) => new this.maps.LatLng(lat, lng));
+    };
+
+    renderDirections = ({ path }) => {
+        const directionsService = new this.maps.DirectionsService();
+        const directionsRenderer = new this.maps.DirectionsRenderer();
+
+        directionsRenderer.setMap(this.gMap);
+
+        const positions = this.interpolatePositions(path);
+        const waypoints = positions
+            .slice(1, positions.length - 1)
+            .map(location => ({ location, stopover: false }));
+
+        const req = {
+            origin: positions[0],
+            destination: positions[positions.length - 1],
+            optimizeWaypoints: true,
+            travelMode: this.maps.TravelMode.DRIVING,
+            waypoints
+        };
+
+        // get the route from directionService and then plot with the help of directionsRenderer
+        directionsService.route(req, (res, flag) => {
+            if (flag === this.maps.DirectionsStatus.OK) {
+                directionsRenderer.setDirections(res);
+            } else {
+                alert('Error in direction service response');
+            }
+        });
+    }
+
     render() {
         return (
             <div className="routeMap-container">
