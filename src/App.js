1315beba32a80ca44f2044a27deaa2ebf5590007
Ashish <ashishrodio@gmail.com>
2019-01-21 01:56:08 +0530

    Added Root component as Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Routes from './routes/Routes';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Routes />
       </div>
     );
   }
---
+++ src/routes/Routes.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { RouteMap } from './components';
+
+const Routes = () => (
+    <RouteMap />
+);
+
+export default Routes;
