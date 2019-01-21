a9a182e8821779d6b8338770524ce12cb0cdddd8
Ashish Verma <ashish.verma@nagarro.com>
2019-01-21 15:45:23 +0530

    Updated RouteInfo to render as child component for RouteForm

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,85 +1,87 @@
-import React, { Component } from 'react';
-import { RouteMap, RouteForm, RouteInfo } from './components';
-import { Loader } from '../shared/loader';
-import { fetchDirections } from './services';
+import React, { Component } from "react";
+import { RouteMap, RouteForm, RouteInfo } from "./components";
+import { Loader } from "../shared/loader";
+import { fetchDirections } from "./services";
 
-import './Routes.css';
+import "./Routes.css";
 /**
  * Acts as a root component
  */
 class Routes extends Component {
+  state = {
+    isLoading: false,
+    directionsApiResponse: null
+  };
 
-    state = {
-        isLoading: false,
-        directionsApiResponse: null
+  // Responsible for show/hide of loader
+  toggleLoader = isLoading => {
+    this.setState(() => ({
+      isLoading
+    }));
+  };
+
+  // propmpts user with error notification
+  displayErrorMessage = msg => {
+    this.toggleLoader(false);
+    this.setState(() => ({
+      directionsApiResponse: null
+    }));
+    alert(msg);
+  };
+
+  // Helps in resetting Application to Initial state
+  resetValues = () => {
+    this.setState({
+      isLoading: false,
+      directionsApiResponse: null
+    });
+  };
+
+  // Makes backend call and updates accordingy
+  getRoutes = async (from, to) => {
+    this.toggleLoader(true);
+
+    // perform network call
+    const response = await fetchDirections(from, to).catch(e => {
+      this.displayErrorMessage("Internal server error");
+    });
+
+    this.toggleLoader(false);
+
+    if (response && response.error) {
+      this.displayErrorMessage(response.error);
+      return;
     }
 
-    // Responsible for show/hide of loader
-    toggleLoader = isLoading => {
-        this.setState(() => ({
-            isLoading
-        }));
-    };
-
-    // propmpts user with error notification
-    displayErrorMessage = msg => {
-        this.toggleLoader(false);
-        this.setState(() => ({
-            directionsApiResponse: null
-        }));
-        alert(msg);
-    };
-
-    // Helps in resetting Application to Initial state
-    resetValues = () => {
-        this.setState({
-            isLoading: false,
-            directionsApiResponse: null
-        })
+    // if response is success update state accordingly
+    if (response && response.path) {
+      this.setState(() => ({
+        directionsApiResponse: response
+      }));
     }
+  };
 
-    // Makes backend call and updates accordingy
-    getRoutes = async (from, to) => {
-        this.toggleLoader(true);
-
-        // perform network call
-        const response = await fetchDirections(from, to).catch(e => {
-            this.displayErrorMessage('Internal server error');
-        });
-
-        this.toggleLoader(false);
-
-        if (response && response.error) {
-            this.displayErrorMessage(response.error);
-            return;
-        }
-
-        // if response is success update state accordingly
-        if (response && response.path) {
-            this.setState(() => ({
-                directionsApiResponse: response
-            }));
-        }
-    }
-
-    render() {
-        let { isLoading, directionsApiResponse } = this.state;
-        return (
-            <div className="container">
-                <Loader isLoading={isLoading} />
-                <div className="route-details">
-                    <RouteForm getRoutes={this.getRoutes} resetValues={this.resetValues} />
-                    {directionsApiResponse && <RouteInfo
-                        totalDistance={directionsApiResponse.total_distance}
-                        totalTime={directionsApiResponse.total_time}
-                    />}
-                </div>
-                <div className="route-map">
-                    <RouteMap directions={directionsApiResponse} />
-                </div>
-            </div>
-        );
-    }
+  render() {
+    let { isLoading, directionsApiResponse } = this.state;
+    return (
+      <div className="container">
+        <Loader isLoading={isLoading} />
+        <div className="route-details">
+          <RouteForm getRoutes={this.getRoutes} resetValues={this.resetValues}>
+            {directionsApiResponse && (
+              <RouteInfo
+                totalDistance={directionsApiResponse.total_distance}
+                totalTime={directionsApiResponse.total_time}
+              />
+            )}
+          </RouteForm>
+        </div>
+        <div className="route-map">
+          <RouteMap directions={directionsApiResponse} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Routes;
--- src/routes/components/RouteForm/RouteForm.js
+++ src/routes/components/RouteForm/RouteForm.js
@@ -1,83 +1,119 @@
-import React, { Component } from 'react';
-import { maps } from '../../../shared/services/maps';
+import React, { Component } from "react";
+import { maps } from "../../../shared/services/maps";
 
-import './RouteForm.css';
+import "./RouteForm.css";
 
 /**
  * Responsible for rendering Route form
  */
 class RouteForm extends Component {
+  state = {
+    btnDisabled: true,
+    submitBtnLabel: "Submit"
+  };
 
-    state = {
-        btnDisabled: true
+  // saves reference of start location input field to trigger map events
+  startInput = React.createRef();
+
+  // saves reference of drop location input field to trigger map events
+  dropInput = React.createRef();
+
+  // setup google Autocomplete on input fields as to add locations
+  setupAutoComplete = async () => {
+    const maps = await this.props.maps();
+    this.setupStartInputAutoComplete = new maps.places.Autocomplete(
+      this.startInput
+    );
+    this.setupDropInputAutoComplete = new maps.places.Autocomplete(
+      this.dropInput
+    );
+  };
+
+  // on form submit ask lng, lat from backend
+  getRoute = () => {
+    const from = this.setupStartInputAutoComplete.getPlace();
+    const to = this.setupDropInputAutoComplete.getPlace();
+    this.props.getRoutes(from, to);
+    this.setState({ submitBtnLabel: "Re-Submit" });
+  };
+
+  componentDidMount() {
+    this.setupAutoComplete();
+  }
+
+  // checks for btn state enable/disbale based on input fields
+  btnStateHandler = () => {
+    if (this.startInput.value.length && this.dropInput.value.length) {
+      this.setState({ btnDisabled: false });
+    } else {
+      this.setState({ btnDisabled: true });
     }
+  };
 
-    // saves reference of start location input field to trigger map events
-    startInput = React.createRef();
+  disableButton = () => {
+    this.setState({ btnDisabled: true });
+    this.props.resetValues();
+  };
 
-    // saves reference of drop location input field to trigger map events
-    dropInput = React.createRef();
-
-    // setup google Autocomplete on input fields as to add locations
-    setupAutoComplete = async () => {
-        const maps = await this.props.maps();
-        this.setupStartInputAutoComplete = new maps.places.Autocomplete(this.startInput);
-        this.setupDropInputAutoComplete = new maps.places.Autocomplete(this.dropInput);
-    }
-
-    // on form submit ask lng, lat from backend
-    getRoute = () => {
-        const from = this.setupStartInputAutoComplete.getPlace();
-        const to = this.setupDropInputAutoComplete.getPlace();
-        this.props.getRoutes(from, to);
-    }
-
-    componentDidMount() {
-        this.setupAutoComplete();
-    }
-
-    // checks for btn state enable/disbale based on input fields
-    btnStateHandler = () => {
-        if(this.startInput.value.length && this.dropInput.value.length) {
-            this.setState({btnDisabled: false})
-        } else {
-            this.setState({btnDisabled: true})
-        }
-    }
-
-    disableButton = () => {
-        this.setState({btnDisabled: true});
-        this.props.resetValues();
-    }
-
-    render() {
-        return (
-            <div className="route-form">
-                <form className="form-input">
-                    <label>Starting Location</label>
-                    <div className="form-input-controls">
-                        <input type="text" ref={el => (this.startInput = el)} onChange={this.btnStateHandler}/>
-                        <button type="reset" onClick={this.disableButton}>X</button>
-                    </div>
-                </form>
-                <form className="form-input">
-                    <label>Drop-off point</label>
-                    <div className="form-input-controls">
-                        <input type="text" ref={el => (this.dropInput = el)} onChange={this.btnStateHandler} />
-                        <button type="reset" onClick={this.disableButton}>X</button>
-                    </div>
-                </form>
-                <div className="get-route-btn">
-                    <button onClick={this.getRoute} disabled={this.state.btnDisabled}>Get Route</button>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    const { btnDisabled, submitBtnLabel } = this.state;
+    return (
+      <div className="route-form">
+        <form className="form-input">
+          <label>Starting Location</label>
+          <div className="form-input-controls">
+            <input
+              type="text"
+              ref={el => (this.startInput = el)}
+              onChange={this.btnStateHandler}
+            />
+            <button type="reset" onClick={this.disableButton}>
+              X
+            </button>
+          </div>
+        </form>
+        <form className="form-input">
+          <label>Drop-off point</label>
+          <div className="form-input-controls">
+            <input
+              type="text"
+              ref={el => (this.dropInput = el)}
+              onChange={this.btnStateHandler}
+            />
+            <button type="reset" onClick={this.disableButton}>
+              X
+            </button>
+          </div>
+        </form>
+        <div className="info-container">{this.props.children}</div>
+        <div className="get-route-btn">
+          <button
+            className="submit-btn"
+            onClick={this.getRoute}
+            disabled={btnDisabled}
+          >
+            {submitBtnLabel}
+          </button>
+          <button
+            className="reset-btn"
+            type="reset"
+            onClick={() => {
+              this.startInput.value = "";
+              this.dropInput.value = "";
+              this.disableButton();
+            }}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 // bind map instance to this component
 RouteForm.defaultProps = {
-    maps
-}
+  maps
+};
 
 export default RouteForm;
