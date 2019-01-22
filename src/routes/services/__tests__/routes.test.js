e6f05278c6df4001e5fee16977789571d6ccbecb
Ashish Verma <ashish.verma@nagarro.com>
2019-01-22 20:21:23 +0530

    updated failure test cases for token and route service calls

--- src/routes/services/__tests__/routes.test.js
+++ src/routes/services/__tests__/routes.test.js
@@ -12,45 +12,45 @@ const FETCH_ROUTE_RESPONSE = {
   total_time: 1800
 };
 
+const FETCH_ROUTE_FAILURE_RESPONSE = {
+  status: "failure",
+  error: "Location not accessible by car"
+};
+
 const FETCH_TOKEN_RESPONSE = {
   token: "token"
 };
 
 describe("API calls check", () => {
-  it("request token from backend", async () => {
-    const postCall = jest.spyOn(makeNetworkCall, "post");
+  let get, post;
 
-    postCall.mockImplementation(() =>
-      Promise.resolve({ data: FETCH_TOKEN_RESPONSE })
-    );
-
-    const token = await fetchToken("from", "to");
-    expect(token).toBeDefined();
-    postCall.mockRestore();
-  });
-
-  it("request location points from backend", async () => {
-    const getCall = jest.spyOn(makeNetworkCall, "get");
-
-    getCall.mockImplementation(() =>
-      Promise.resolve({ data: FETCH_ROUTE_RESPONSE })
-    );
-
-    const result = await fetchRoute("dummyTokenValue");
-    expect(result).toBeDefined();
-    getCall.mockRestore();
-  });
-
-  it("requests location details with token and lng/lat", async () => {
-    const get = jest.spyOn(makeNetworkCall, "get");
-    const post = jest.spyOn(makeNetworkCall, "post");
+  beforeEach(() => {
+    get = jest.spyOn(makeNetworkCall, "get");
+    post = jest.spyOn(makeNetworkCall, "post");
 
     post.mockImplementation(() =>
       Promise.resolve({
         data: FETCH_TOKEN_RESPONSE
       })
     );
+  });
 
+  it("request token from backend", async () => {
+    const token = await fetchToken("from", "to");
+    expect(token).toBeDefined();
+    post.mockRestore();
+  });
+
+  it("request location points from backend", async () => {
+    get.mockImplementation(() =>
+      Promise.resolve({ data: FETCH_ROUTE_RESPONSE })
+    );
+    const result = await fetchRoute("dummyTokenValue");
+    expect(result).toBeDefined();
+    get.mockRestore();
+  });
+
+  it("requests location details with token and lng/lat", async () => {
     get.mockImplementation(() =>
       Promise.resolve({
         data: FETCH_ROUTE_RESPONSE
@@ -60,5 +60,20 @@ describe("API calls check", () => {
     const result = await fetchDirections("from", "to");
     expect(result).toBeDefined();
     expect(result.status).toEqual("success");
+    get.mockRestore();
+  });
+
+  it("handles faliure responses while requesting location details with token and lng/lat", async () => {
+    get.mockImplementation(() =>
+      Promise.resolve({
+        data: FETCH_ROUTE_FAILURE_RESPONSE
+      })
+    );
+
+    const result = await fetchDirections("from", "to");
+    expect(result).toBeDefined();
+    expect(result.status).toEqual("failure");
+    expect(result.error).toEqual("Location not accessible by car");
+    get.mockRestore();
   });
 });
--- src/routes/services/routes/routes.js
+++ src/routes/services/routes/routes.js
@@ -1,38 +1,38 @@
-import { makeNetworkCall } from '../../../shared/services/axios';
+import { makeNetworkCall } from "../../../shared/services/axios";
 
 // Responsible for fetching token value
 const fetchToken = async (from, to) => {
-    const request = {
-        from,
-        to
-    };
-    const response = await makeNetworkCall.post("/route", request);
-    const { token } = response.data;
-    return token;
+  const request = {
+    from,
+    to
+  };
+  const response = await makeNetworkCall.post("/route", request);
+  const { token } = response.data;
+  return token;
 };
 
 // Responsible for fetching Route if token value available
 const fetchRoute = async token => {
-    const response = await makeNetworkCall.get(`/route/${token}`);
-    return response.data;
+  const response = await makeNetworkCall.get(`/route/${token}`);
+  return response.data;
 };
 
 // Handle server response and retry if server busy
 const fetchDirections = async (from, to) => {
-    const token = await fetchToken(from, to);
-    let result = await fetchRoute(token);
+  const token = await fetchToken(from, to);
+  let result = await fetchRoute(token);
 
-    // if status is 'in progress' then retry the request again
-    if (
-        result &&
-        result.status &&
-        result.status.toLowerCase() === "in progress"
-    ) {
-        // calls itself again if server busy
-        result = await fetchDirections(from, to);
-    }
+  // if status is 'in progress' then retry the request again
+  if (
+    result &&
+    result.status &&
+    result.status.toLowerCase() === "in progress"
+  ) {
+    // calls itself again if server busy
+    result = await fetchDirections(from, to);
+  }
 
-    return result;
+  return result;
 };
 
 export { fetchToken, fetchRoute, fetchDirections };
