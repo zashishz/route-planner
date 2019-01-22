0b880ca0dbb1c0954b917d759457860d2ead51c6
Ashish Verma <ashish.verma@nagarro.com>
2019-01-22 18:25:22 +0530

    Updated Loader Component Name

---
+++ src/shared/Loader/Loader.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./loader.css";
+
+// displays loader on screen if isLoading flag is set true
+function Loader({ isLoading }) {
+  return (
+    isLoading && (
+      <div className="loader-container">
+        <div className="lds-ripple">
+          <div />
+          <div />
+        </div>
+      </div>
+    )
+  );
+}
+
+export default Loader;
